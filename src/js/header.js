46d9338dd4346daa00b502e052c55f57c903c031
1ster14ka <danyazybastik@gmail.com>
2024-11-22 00:43:49 +0200

    head is readyyy

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,35 @@
-const burger = document.querySelector('.icon-burger');
-const modal = document.querySelector('.modal-overlay');
-const close = document.querySelector('.icon-close');
-burger.addEventListener('click', event => {
-  event.preventDefault();
-  modal.classList.add('active');
-  burger.removeEventListener();
+// const burger = document.querySelector('.burger-btn');
+// const modal = document.querySelector('.modal-overlay');
+// const close = document.querySelector('.icon-x');
+// burger.addEventListener('click', event => {
+//   event.preventDefault();
+//   modal.classList.add('active');
+//   burger.removeEventListener();
+// });
+
+// close.addEventListener('click', () => {
+//   modal.classList.remove('active');
+// });
+
+const burgerBtn = document.querySelector('.burger-btn'); // Кнопка бургер
+const modalOverlay = document.querySelector('.modal-overlay'); // Модалка
+const closeModalBtn = modalOverlay.querySelector('.icon-close'); // Крестик в модалке
+
+// Открыть модалку
+burgerBtn.addEventListener('click', () => {
+  modalOverlay.classList.add('active'); // Добавляем класс для отображения модалки
+  // burgerBtn.style.display = 'none';
 });
 
-close.addEventListener('click', () => {
-  modal.classList.remove('active');
+// Закрыть модалку
+closeModalBtn.addEventListener('click', () => {
+  modalOverlay.classList.remove('active'); // Убираем класс для скрытия модалки
+  // burgerBtn.style.display = 'flex';
+});
+
+// Закрытие модалки по клику вне её области
+modalOverlay.addEventListener('click', event => {
+  if (event.target === modalOverlay) {
+    modalOverlay.classList.remove('active');
+  }
 });
